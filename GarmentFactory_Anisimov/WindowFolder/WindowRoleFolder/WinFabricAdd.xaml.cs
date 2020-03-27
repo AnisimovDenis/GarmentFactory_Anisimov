8f3b58022b29dfad41cd30afe3d8422d00090b1a
Denis Anisimov <den.anisimov.94@mail.ru>
2020-03-27 10:15:05 +0300

    Разработал добавление ткани

--- GarmentFactory_Anisimov/WindowFolder/WindowRoleFolder/WinFabricAdd.xaml.cs
+++ GarmentFactory_Anisimov/WindowFolder/WindowRoleFolder/WinFabricAdd.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GarmentFactory_Anisimov.ClassFolder;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -33,13 +34,76 @@ namespace GarmentFactory_Anisimov.WindowFolder.WindowRoleFolder
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-            connection.Open();
-            cmd = new SqlCommand("INSERT INTO [User]" +
-                "VALUES (@Login, @Password, @RoleId)", connection);
-            cmd.Parameters.AddWithValue("Login", tbLogin.Text);
-            cmd.Parameters.AddWithValue("Password", pbPassword.Password);
-            cmd.Parameters.AddWithValue("RoleId", 1);
-            cmd.ExecuteNonQuery();
+            if (string.IsNullOrEmpty(tbArticle.Text))
+            {
+                ClassMessageBox.MessageBoxError("Введите артикул");
+                tbArticle.Focus();
+            }
+            else if (string.IsNullOrEmpty(tbColor.Text))
+            {
+                ClassMessageBox.MessageBoxError("Введите цвет");
+                tbColor.Focus();
+            }
+            else if (string.IsNullOrEmpty(tbComposition.Text))
+            {
+                ClassMessageBox.MessageBoxError("Введите композицию");
+                tbComposition.Focus();
+            }
+            else if (string.IsNullOrEmpty(tbDrawing.Text))
+            {
+                ClassMessageBox.MessageBoxError("Введите узор");
+                tbDrawing.Focus();
+            }
+            else if (string.IsNullOrEmpty(tbLength.Text))
+            {
+                ClassMessageBox.MessageBoxError("Введите длину");
+                tbLength.Focus();
+            }
+            else if (string.IsNullOrEmpty(tbName.Text))
+            {
+                ClassMessageBox.MessageBoxError("Введите наименование");
+                tbName.Focus();
+            }
+            else if (string.IsNullOrEmpty(tbPrice.Text))
+            {
+                ClassMessageBox.MessageBoxError("Введите цену");
+                tbPrice.Focus();
+            }
+            else if (string.IsNullOrEmpty(tbWidth.Text))
+            {
+                ClassMessageBox.MessageBoxError("Введите ширину");
+                tbWidth.Focus();
+            }
+            else
+            {
+                try
+                {
+                    connection.Open();
+                    cmd = new SqlCommand("INSERT INTO [Fabric] " +
+                        "(ArticleNumber, Name, Color, Drawing, " +
+                        "Composition, Width, Length, Price)" +
+                        "VALUES (@ArticleNumber, @Name," +
+                        " @Color, @Drawing, @Composition, " +
+                        "@Width, @Length, @Price)", connection);
+                    cmd.Parameters.AddWithValue("ArticleNumber", tbArticle.Text);
+                    cmd.Parameters.AddWithValue("Name", tbName.Text);
+                    cmd.Parameters.AddWithValue("Color", tbColor.Text);
+                    cmd.Parameters.AddWithValue("Drawing", tbDrawing.Text);
+                    cmd.Parameters.AddWithValue("Composition", tbComposition.Text);
+                    cmd.Parameters.AddWithValue("Width", tbWidth.Text);
+                    cmd.Parameters.AddWithValue("Length", tbLength.Text);
+                    cmd.Parameters.AddWithValue("Price", tbPrice.Text);
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    ClassMessageBox.MessageBoxError(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
         }
     }
 }
